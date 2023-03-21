70af285f552a7edeac38b541c1e1b91f417bf3ba
Saad Khan <saadnasirkhan@gmail.com>
2023-03-21 10:43:31 -0700

    new code

--- spyn.m
+++ spyn.m
@@ -1,102 +1,91 @@
 global key
 
-% brick = ConnectBrick('H')
+fwspeed = -40
+bwspeed = 40
+motors = 'AB'
+pick = 'D'
+dt = 30
 
-% brick.SetColorMode(2, 2);
+while 1
 
-% 1	Black
-% 2	Blue
-% 3	Green
-% 4	Yellow
-% 5	Red
-% 6	White
-% 7	 Brown
+    brick.MoveMotor(motors, fwspeed);
 
-while true
-
-touch = brick.TouchPressed(1);
-color = brick.ColorCode(2);
-distance = brick.UltrasonicDist(3);
-
-    if color == 1
-    
-        if ~touch && distance > 10
-            brick.MoveMotor('AB', -20);
-    
-        else
-            brick.MoveMotor('AB', 0);
-    
-            if brick.UltrasonicDist(3) > 10
-                brick.MoveMotor('AB', 20);
-                pause(2);
-                brick.MoveMotor('AB', 0);
-                brick.MoveMotor('A', -20);
-                brick.MoveMotor('B', 10);
-                pause(3);
-                brick.MoveMotor('AB', 0)
-            else
-                brick.MoveMotor('AB', 20);
-                pause(2);
-                brick.MoveMotor('AB', 0);
-                brick.MoveMotor('B', -20);
-                brick.MoveMotor('A', 10);
-                pause(3);
-                brick.MoveMotor('AB', 0)
-    
-            end
-        end
-    
-        elseif color == 5
-            brick.StopMotor('AB', 'Coast');
-            pause(2);
-            brick.MoveMotor('AB', 20);
-            pause(4);
-    
-        elseif color == 4 || color == 3
-            brick.StopMotor('AB', 'Coast');
-    
-            InitKeyboard();
-    
-            while 1
-                pause(0.1);
-    
-                switch key
-    
-                    case 'uparrow'
-    
-                        brick.MoveMotor('AB', -20);
-    
-                    case 'downarrow'
-    
-                        brick.MoveMotor('AB', 20)
-    
-                    case 'leftarrow'
-    
-                        brick.MoveMotor('A', 40)
-                        brick.MoveMotor('B', -20)
-    
-                    case 'rightarrow'
-    
-                        brick.MoveMotor('B', 40)
-                        brick.MoveMotor('A', -20)
-    
-                    case 0
-    
-                        brick.StopMotor('AB', 'Coast')
-    
-                    case 'u'
-    
-                        brick.MoveMotorAngleRel('D', 10, 10, 'Brake')
-    
-                    case 'i'
-    
-                        brick.MoveMotorAngleRel('D', 10, -10, 'Brake')
-    
-                    case 'q'
-    
-                        break
-                end
-            end
-            CloseKeyboard();
+    if distance > dt
+        brick.StopMotor(motors, 'Coast');
+        brick.MoveMotor('A', -30);
+        brick.MoveMotor('B', 10);
+        pause(2)
+        brick.StopMotor(motors, 'Coast');
+        brick.MoveMotor(motors, fwspeed);
     end
-end
+
+    if touch
+
+        brick.StopMotor(motors, 'Coast');
+        brick.MoveMotor(motors, bwspeed);
+        pause(0.5);
+        brick.StopMotor(motors, 'Coast');
+        brick.MoveMotor('A', 10);
+        brick.MoveMotor('B', -30);
+        pause(2);
+        brick.StopMotor(motors, 'Coast');
+        brcik.MoveMotor(motors, fwspeed);
+    end
+
+    if color == 5
+        brick.StopMotor(motors, 'Coast');
+        pause(3);
+        brick.MoveMotor(motors, fwspeed);
+        pause(2);
+
+    elseif color == 4 || color == 3
+
+        brick.StopMotor('AB', 'Coast');
+
+        InitKeyboard();
+
+        while 1
+            pause(0.1);
+
+            switch key
+
+                case 'uparrow'
+
+                    brick.MoveMotor(motors, fwspeed);
+
+                case 'downarrow'
+
+                    brick.MoveMotor(motors, bwspeed);
+
+                case 'leftarrow'
+
+                    brick.MoveMotor('A', 30);
+                    brick.MoveMotor('B', -10);
+
+                case 'rightarrow'
+
+                    brick.MoveMotor('A', -10);
+                    brick.MoveMotor('B', 30);
+
+                case 0
+
+                    brick.StopMotor(motors, 'Coast');
+                    brick.StopMotor('D', 'Brake')
+
+                case 'w'
+
+                    brick.MoveMotor('D', -10)
+
+                case 'd'
+
+                    brick.MoveMotor('D', 10)
+
+                case 'q'
+
+                    break;
+            end
+
+        end
+
+        CloseKeyboard();
+    end
